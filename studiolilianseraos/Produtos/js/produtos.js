19de4e7a361d27919098da6a4de2ee2ab6957b16
Cecilia Sousa <ceciliasousa21@gmail.com>
2019-10-24 12:18:51 -0300

    sobe pagina da cliente lilian seraos

---
+++ studiolilianseraos/Produtos/js/produtos.js
@@ -0,0 +1,93 @@
+// // MDB Lightbox Init
+// $(function () {
+//     $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+// });
+
+/////////////////////////////////
+
+let modalId = $('#image-gallery');
+
+$(document)
+    .ready(function () {
+
+        loadGallery(true, 'a.thumbnail');
+
+        function disableButtons(counter_max, counter_current) {
+            $('#show-previous-image, #show-next-image')
+                .show();
+            if (counter_max === counter_current) {
+                $('#show-next-image')
+                    .hide();
+            } else if (counter_current === 1) {
+                $('#show-previous-image')
+                    .hide();
+            }
+        }
+
+        
+        function loadGallery(setIDs, setClickAttr) {
+            let current_image,
+                selector,
+                counter = 0;
+
+            $('#show-next-image, #show-previous-image')
+                .click(function () {
+                    if ($(this)
+                        .attr('id') === 'show-previous-image') {
+                        current_image--;
+                    } else {
+                        current_image++;
+                    }
+
+                    selector = $('[data-image-id="' + current_image + '"]');
+                    updateGallery(selector);
+                });
+
+            function updateGallery(selector) {
+                let $sel = selector;
+                current_image = $sel.data('image-id');
+                $('#image-gallery-title')
+                    .text($sel.data('title'));
+                $('#image-gallery-image')
+                    .attr('src', $sel.data('image'));
+                disableButtons(counter, $sel.data('image-id'));
+            }
+
+            if (setIDs == true) {
+                $('[data-image-id]')
+                    .each(function () {
+                        counter++;
+                        $(this)
+                            .attr('data-image-id', counter);
+                    });
+            }
+            $(setClickAttr)
+                .on('click', function () {
+                    updateGallery($(this));
+                });
+        }
+    });
+
+
+$(document)
+    .keydown(function (e) {
+        switch (e.which) {
+            case 37: // left
+                if ((modalId.data('bs.modal') || {})._isShown && $('#show-previous-image').is(":visible")) {
+                    $('#show-previous-image')
+                        .click();
+                }
+                break;
+
+            case 39: // right
+                if ((modalId.data('bs.modal') || {})._isShown && $('#show-next-image').is(":visible")) {
+                    $('#show-next-image')
+                        .click();
+                }
+                break;
+
+            default:
+                return; // exit this handler for other keys
+        }
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
---
+++ studiolilianseraos/README.md
@@ -0,0 +1,3 @@
+# Projeto com clientes reais
+
+Site criado na {reprograma} para a cliente Studio Lilian Seraos Arte e Design
